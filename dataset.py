4f491b6df7e4ee9ba1d8234288061064e0089a30
pingyian1133 <1060346692pya@gmail.com>
2021-07-25 13:16:36 +0800

    dataloader

--- dataset.py
+++ dataset.py
@@ -38,14 +38,18 @@ class BaseDataLoader:
     @staticmethod
     def collate_fn(batch):
         x_list = []
+        x_len_list = []
         y_list = []
         for index, (x, y) in enumerate(batch):
             x_list.append(torch.tensor(x).float())
+            x_len_list.append(torch.tensor(x).size(0))
             y_list.append(torch.tensor(y).long())
         padded_x = torch.nn.utils.rnn.pad_sequence(x_list, batch_first=True)
         padded_y = torch.nn.utils.rnn.pad_sequence(y_list, batch_first=True)
+        x_len = torch.tensor(x_len_list)
+        # print(padded_x)
         # y = torch.stack(y_list, dim=0)
-        return padded_x, padded_y
+        return padded_x, x_len, padded_y
 
     def get_loader(self, for_what):
         train_dataset = T4SDataset(self.config, self.train_df)
@@ -62,8 +66,10 @@ if __name__ == '__main__':
     dl = BaseDataLoader(config)
     tl = dl.get_loader('train')
     print(tl)
-    for i, (x, y) in enumerate(tl):
+    for i, (x, x_len, y) in enumerate(tl):
         print(i)
-        print(x.shape)
-        print(y)
-        break
+        print(x.shape)  # torch.Size([batch, 2974, 160])
+        print(x_len)
+        print(y.size())
+        if i == 3:
+            break
